ad038586ac06e06af95586ae9e31dc8f101fc5e5
Kenneth Thomsen <contact@systemedz.com>
2023-06-13 11:59:36 +0200

    - Removed Docker Compose - Added test endpoint

---
+++ src/main/java/dk/addvanced/docker/TestController.java
@@ -0,0 +1,16 @@
+package dk.addvanced.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public @ResponseBody String helloWorld() {
+        return "Hello there..";
+    }
+}
