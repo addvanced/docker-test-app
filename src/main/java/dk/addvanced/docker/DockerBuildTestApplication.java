f10806c3d36dbac01858488d18f73690bd32aa7c
Kenneth Thomsen <contact@systemedz.com>
2023-06-13 11:29:29 +0200

    Initial Commit

---
+++ src/main/java/dk/addvanced/docker/DockerBuildTestApplication.java
@@ -0,0 +1,13 @@
+package dk.addvanced.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBuildTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBuildTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/addvanced/docker/DockerBuildTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.addvanced.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBuildTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
